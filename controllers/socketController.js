243e844fba8cba6eaaa3bddaf0af536f2c4e3747
faerlinpulido <pulido.faerlin@gmail.com>
2021-01-21 18:41:34 -0500

    catchAsync on socketController functions

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,14 +1,9 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const Message = require('../models/messageModel');
+const catchAsync = require('../util/catchAsync');
 
-exports.disconnect = socket => {
-    socket.on('disconnect', async () => {
-        console.log(`user disconnected with id: ${socket.id}`);
-    });
-};
-
-exports.authenticate = async (socket, next) => {
+exports.authenticate = catchAsync(async (socket, next) => {
 
     if (socket.handshake.query && socket.handshake.query.jwt) {
         const decoded = jwt.verify(socket.handshake.query.jwt, process.env.JWT_SECRET);
@@ -19,26 +14,32 @@ exports.authenticate = async (socket, next) => {
     }
 
     next(new Error('Connection failed'));
+});
+
+exports.disconnect = socket => {
+    socket.on('disconnect', catchAsync(async () => {
+        console.log(`user disconnected with id: ${socket.id}`);
+    }));
 };
 
 exports.message = (io, socket) => {
-    socket.on('message', async (message) => {
+    socket.on('message', catchAsync(async (message) => {
         await Message.create(message);
         io.in(`chat:${message.from}`).emit('message', message);
         io.in(`chat:${message.to}`).emit('message', message);
-    });
+    }));
 }
 
 exports.find = socket => {
-    socket.on('find', async (query) => {
+    socket.on('find', catchAsync(async (query) => {
         const results = await User.find({ username: new RegExp(query.trim(), 'i') }, { '_id': 1, 'username': 1 });
         socket.emit('find', results);
-    });
+    }));
 };
 
 exports.join = socket => {
-    socket.on('join', async (room) => {
+    socket.on('join', catchAsync(async (room) => {
         socket.join(room);
         console.log(`Socket ID ${socket.id} joined ${room}`);
-    });
+    }));
 };
